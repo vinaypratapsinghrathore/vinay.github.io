fe2fc78f9260c1d87cbd9c27918fdf20e954558f
viany <rathoreyuvrajsingh38@gmail.com>
2023-09-24 15:57:37 +0530

    vinay

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  // navigation bar toggle
+  $("#navbar-toggler").click(function () {
+    $(".navbar-collapse").slideToggle(400);
+  });
+  //navbar bg change on scroll
+  $(window).scroll(function () {
+    let pos = $(window).scrollTop();
+    if (pos >= 100) {
+      $(".navbar").addClass("cng-navbar");
+    } else {
+      $(".navbar").removeClass("cng-navbar");
+    }
+  });
+});
